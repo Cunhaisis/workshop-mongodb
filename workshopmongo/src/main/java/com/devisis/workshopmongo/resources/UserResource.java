aa58d004634e456efdcd4736501bcdcb3d4524a4
Cunhaisis <isis.cunha13@gmail.com>
2020-11-24 19:03:29 -0300

    Atualização de usuário com PUT

--- workshopmongo/src/main/java/com/devisis/workshopmongo/resources/UserResource.java
+++ workshopmongo/src/main/java/com/devisis/workshopmongo/resources/UserResource.java
@@ -58,4 +58,13 @@ public class UserResource {
 
 	}
 	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDto objDto, @PathVariable String id ) {
+		User obj = service.fromDto(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+
+	}
+	
 }
